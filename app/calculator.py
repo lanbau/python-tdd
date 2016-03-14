1e5b762a20e9f6c5b1bb14b696c11310a28ff7d7
lanbau <irvinln@gmail.com>
2016-03-15 06:22:37 +0800

    initial commit

---
+++ app/calculator.py
@@ -0,0 +1,10 @@
+class Calculator(object):
+    def add(self, x, y):
+        number_types = (int, long, float, complex)
+
+        if isinstance(x, number_types) and isinstance(y, number_types):
+            # import pdb; pdb.set_trace()
+            # return x - y
+            return x + y
+        else:
+            raise ValueError
